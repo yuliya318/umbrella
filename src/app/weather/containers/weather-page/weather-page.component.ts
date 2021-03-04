2ca3830139beb15f48008a458c66320f2e81a134
yuliya318 <yulya31087@gmail.com>
2021-03-04 23:59:42 +0200

    adding responsive styles

--- src/app/weather/containers/weather-page/weather-page.component.ts
+++ src/app/weather/containers/weather-page/weather-page.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-page.component.scss'],
 })
 export class WeatherPageComponent implements OnInit {
+  public iconUrl = 'http://openweathermap.org/img/wn/10d@2x.png';
   
   constructor() {}
 
