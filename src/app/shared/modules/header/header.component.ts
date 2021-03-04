b0affa801d9d50173d6c8c8963b02d047710be58
yuliya318 <yulya31087@gmail.com>
2021-03-05 01:03:33 +0200

    separating the header into a module

---
+++ src/app/shared/modules/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  public city: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
